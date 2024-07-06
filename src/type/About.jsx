f72cd6a077df31ce318d2fb9251238f39a59ce55
jllee000 <jllee000@naver.com>
2024-07-06 18:03:03 +0900

    [에고이스트] 유형테스트 번아웃

--- src/type/About.jsx
+++ src/type/About.jsx
@@ -3,17 +3,23 @@ import styles from "./type.css";
 export default function About(){
     return(
         <div className="aboutpage">
-            <div className="about-title">
-                <img src="/images/intro/type1_title.png" className="img-width" alt="image"/>
-            </div>
-            <div className="about-icon">
-                <img src="/images/intro/type1_icon.png" className="img-width" alt="image"/>
-            </div>
-            <div className="about-txt">
-                <img src="/images/intro/about_type1.png" className="img-width" alt="image"/>
-            </div>
-            <div className="aboutpage-btn">
-                선택하기
+            <div className="aboutpage-main">
+                <div className="aboutpage-detail">
+                    <div className="about-title">
+                        <img src="/images/intro/type1_title.png" className="img-width" alt="image"/>
+                    </div>
+                    <div className="about-icon">
+                        <img src="/images/intro/type1_icon.png" className="img-width" alt="image"/>
+                    </div>
+                    <div className="about-txt">
+                        <img src="/images/intro/about_type1.png" className="img-width" alt="image"/>
+                    </div>
+                </div>
+                <div className="aboutpage-btn-area">
+                    <div className="aboutpage-btn">
+                        선택하기
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/type/Typepage.jsx
+++ src/type/Typepage.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import styles from "./type.css";
-
+import About from "./About";
 export default function TypePage(){
  return(
     <div className="typepage">
-        <div className="typepage-title">
-            <img src="/images/intro/mytypetitle.png" alt="image" className="img-width"/>
-        </div>
-        <div className="type-select-area">
-            <div className="type-box">
-                <img className="img-width" alt="image" src="/images/intro/type1.png"/>
+        <About />
+        <div className="typepage-main">
+            <div className="typepage-title">
+                <img src="/images/intro/mytypetitle.png" alt="image" className="img-width"/>
             </div>
-            <div className="type-box">
-                <img className="img-width" alt="image" src="/images/intro/type2.png"/>
+            <div className="type-select-area">
+                <div className="type-box">
+                    <img className="img-width" alt="image" src="/images/intro/type1.png"/>
+                </div>
+                <div className="type-box">
+                    <img className="img-width" alt="image" src="/images/intro/type2.png"/>
+                </div>
+                <div className="type-box">
+                    <img className="img-width" alt="image" src="/images/intro/type3.png"/>
+                </div>
+                <div className="type-box">
+                    <img className="img-width" alt="image" src="/images/intro/type4.png"/>
+                </div>
+                <div className="type-box">
+                    <img className="img-width" alt="image" src="/images/intro/type5.png"/>
+                </div>
             </div>
-            <div className="type-box">
-                <img className="img-width" alt="image" src="/images/intro/type3.png"/>
+            <div className="typepage-btn-area">
+                <div className="typepage-btn">다음</div>
             </div>
-            <div className="type-box">
-                <img className="img-width" alt="image" src="/images/intro/type4.png"/>
-            </div>
-            <div className="type-box">
-                <img className="img-width" alt="image" src="/images/intro/type5.png"/>
-            </div>
-        </div>
-        <div className="typepage-btn-area">
-            <div className="typepage-btn">다음</div>
         </div>
     </div>
  );   
