712b4f2f369c1fd03126e096130dc2751386447e
jllee000 <jllee000@naver.com>
2024-07-08 23:26:24 +0900

    0708 인트로 기능작업

--- src/type/About.jsx
+++ src/type/About.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./type.css";
-export default function About(){
-    return(
+
+export default function About({ onSelect, type, isVisible, setIsVisible }) {
+    useEffect(() => {
+        setIsVisible(true);
+    }, [type]);
+
+    const handleClose = () => {
+        setIsVisible(false);
+        onSelect(0);
+    };
+
+    const handleSelect = () => {
+        onSelect(type);
+        setIsVisible(false);
+    };
+
+    if (!isVisible) return null;
+
+    return (
         <div className="aboutpage">
             <div className="aboutpage-main">
+                <div className="aboutpage-close" onClick={handleClose}>
+                    <img src="/images/intro/x.png" alt="close" className="img-width" />
+                </div>
                 <div className="aboutpage-detail">
                     <div className="about-title">
-                        <img src="/images/intro/type1_title.png" className="img-width" alt="image"/>
+                        <img src={`/images/intro/type${type}_title.png`} className="img-width" alt="title" />
                     </div>
                     <div className="about-icon">
-                        <img src="/images/intro/type1_icon.png" className="img-width" alt="image"/>
+                        <img src={`/images/intro/type${type}_icon.png`} className="img-width" alt="icon" />
                     </div>
                     <div className="about-txt">
-                        <img src="/images/intro/about_type1.png" className="img-width" alt="image"/>
+                        <img src={`/images/intro/about_type${type}.png`} className="img-width" alt="description" />
                     </div>
                 </div>
                 <div className="aboutpage-btn-area">
-                    <div className="aboutpage-btn">
+                    <div className="aboutpage-btn" onClick={handleSelect}>
                         선택하기
                     </div>
                 </div>
--- src/type/Typepage.jsx
+++ src/type/Typepage.jsx
@@ -1,35 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./type.css";
 import About from "./About";
-export default function TypePage(){
- return(
-    <div className="typepage">
-        <About />
-        <div className="typepage-main">
-            <div className="typepage-title">
-                <img src="/images/intro/mytypetitle.png" alt="image" className="img-width"/>
-            </div>
-            <div className="type-select-area">
-                <div className="type-box">
-                    <img className="img-width" alt="image" src="/images/intro/type1.png"/>
+
+export default function TypePage() {
+    const [selectedType, setSelectedType] = useState(null);
+    const [aboutType, setAboutType] = useState(null);
+    const [isAboutVisible, setIsAboutVisible] = useState(false);
+
+    const handleSelect = (type) => {
+        if (type === 0) {
+            setAboutType(null);
+        } else {
+            setSelectedType(type);
+            setAboutType(type);
+        }
+    };
+
+    return (
+        <div className="typepage">
+            {aboutType !== null && (
+                <About
+                    onSelect={handleSelect}
+                    type={aboutType}
+                    isVisible={isAboutVisible}
+                    setIsVisible={setIsAboutVisible}
+                />
+            )}
+            <div className="typepage-main">
+                <div className="typepage-title">
+                    <img src="/images/intro/mytypetitle.png" alt="title" className="img-width" />
                 </div>
-                <div className="type-box">
-                    <img className="img-width" alt="image" src="/images/intro/type2.png"/>
+                <div className="type-select-area">
+                    <div
+                        className={`type-box type-box-1 ${selectedType === 1 ? "type-box-1-active" : ""}`}
+                        onClick={() => {
+                            setAboutType(1);
+                            setIsAboutVisible(true);
+                        }}
+                    >
+                        <img className="img-width" alt="type1" src="/images/intro/type1.png" />
+                    </div>
+                    <div
+                        className={`type-box type-box-2 ${selectedType === 2 ? "type-box-2-active" : ""}`}
+                        onClick={() => {
+                            setAboutType(2);
+                            setIsAboutVisible(true);
+                        }}
+                    >
+                        <img className="img-width" alt="type2" src="/images/intro/type2.png" />
+                    </div>
+                    <div
+                        className={`type-box type-box-3 ${selectedType === 3 ? "type-box-3-active" : ""}`}
+                        onClick={() => {
+                            setAboutType(3);
+                            setIsAboutVisible(true);
+                        }}
+                    >
+                        <img className="img-width" alt="type3" src="/images/intro/type3.png" />
+                    </div>
+                    <div
+                        className={`type-box type-box-4 ${selectedType === 4 ? "type-box-4-active" : ""}`}
+                        onClick={() => {
+                            setAboutType(4);
+                            setIsAboutVisible(true);
+                        }}
+                    >
+                        <img className="img-width" alt="type4" src="/images/intro/type4.png" />
+                    </div>
+                    <div
+                        className={`type-box type-box-5 ${selectedType === 5 ? "type-box-5-active" : ""}`}
+                        onClick={() => {
+                            setAboutType(5);
+                            setIsAboutVisible(true);
+                        }}
+                    >
+                        <img className="img-width" alt="type5" src="/images/intro/type5.png" />
+                    </div>
                 </div>
-                <div className="type-box">
-                    <img className="img-width" alt="image" src="/images/intro/type3.png"/>
+                <div className="typepage-btn-area">
+                    <div className="typepage-btn">다음</div>
                 </div>
-                <div className="type-box">
-                    <img className="img-width" alt="image" src="/images/intro/type4.png"/>
-                </div>
-                <div className="type-box">
-                    <img className="img-width" alt="image" src="/images/intro/type5.png"/>
-                </div>
-            </div>
-            <div className="typepage-btn-area">
-                <div className="typepage-btn">다음</div>
             </div>
         </div>
-    </div>
- );   
+    );
 }
