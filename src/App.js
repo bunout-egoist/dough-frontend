30356b81ffad3be57ece18997e5efef4df99c571
jllee000 <jllee000@naver.com>
2024-07-17 17:21:12 +0900

    0717 메인페이지 작업 시작

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Navbar from "./navbar/Navbar";
 import TypePage from "./type/Typepage";
 import Setting from "./settings/Setting";
 import Nickname from "./settings/Nickname";
+import Mainpage from "./main/Mainpage";
 function App() {
   return (
     <div className="common-layout">
       <div className="app-main">
         <Routes>
-          <Route path="/"  element={<Setting/>} />
+          <Route path="/"  element={<Mainpage/>} />
+          <Route path="/setting"  element={<Setting/>} />
           <Route path="/typepage" element={<TypePage/>}/>
           <Route path="/dashpage"  element={<DashPage/>} />
           <Route path="/detail"  element={<DetailPage/>} />
--- src/main/Mainpage.jsx
+++ src/main/Mainpage.jsx
@@ -3,14 +3,14 @@ import styles from "./main.css";
 export default function Mainpage(){
     return(
         <div className="mainpage">
-            <div>오늘 퀘스트는<br/><span className="mission-tag1">#밖에서</span><span className="mission-tag2">혼자</span> 할 수 있어요!</div>
-            <div>
-                <div>레벨 5</div>
+            <div className="mainpage-title">오늘 퀘스트는<br/><span className="mission-tag1">#밖에서</span><span className="mission-tag2">혼자</span> 할 수 있어요!</div>
+            <div className="mainpage-level">
+                <div className="mainpage-level-title">레벨 5</div>
                 <div className="outer">
                     <div className="inner"></div>
                 </div> 
             </div>
-            <div><img src=""/></div>
+            <div className="mainpage-img"><img src="/images/main/main_icon.png" className="img-width"/></div>
             <div>
                 <div></div>
                 <div></div>
