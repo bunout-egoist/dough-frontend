3ccc86312427c2d2cdd8402b645f503ed17e3dc2
jllee000 <jllee000@naver.com>
2024-07-20 15:59:51 +0900

    0720 navbar 에러 수정 및 레이아웃

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import TypePage from "./type/Typepage";
 import Setting from "./settings/Setting";
 import Nickname from "./settings/Nickname";
 import Mainpage from "./main/Mainpage";
+
 function App() {
   return (
     <div className="common-layout">
       <div className="app-main">
-        <Routes>
-          <Route path="/"  element={<Mainpage/>} />
-          <Route path="/setting"  element={<Setting/>} />
-          <Route path="/typepage" element={<TypePage/>}/>
-          <Route path="/dashpage"  element={<DashPage/>} />
-          <Route path="/detail"  element={<DetailPage/>} />
-          <Route path="/nickname"  element={<Nickname/>} />
-        </Routes>
+        <div className="page-area">
+          <Routes>
+            <Route path="/"  element={<Mainpage/>} />
+            <Route path="/setting"  element={<Setting/>} />
+            <Route path="/typepage" element={<TypePage/>}/>
+            <Route path="/dashpage"  element={<DashPage/>} />
+            <Route path="/detail"  element={<DetailPage/>} />
+            <Route path="/nickname"  element={<Nickname/>} />
+          </Routes>
+        </div>
+       <Navbar/>
       </div>
+    
     </div>
   );
 }
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import styles from "./navbar.css"
+import styles from "./navbar.css";
+import { Link } from 'react-router-dom';
 export default function Navbar() {
-    <div className="navbar-area">
-        <div className="navbar-icon">
-            <img src="/images/navbarImage/chart.png" alt="이미지" className="img-width"/>
+    return(
+        <div className="navbar-area">
+            <Link to= "/dashpage">
+                <div className="navbar-icon">
+                    <img src="/images/navbarImage/chart.png" alt="이미지" className="img-width"/>
+                </div>
+            </Link>
+            <Link to="/">
+                <div className="navbar-icon">
+                    <img src="/images/navbarImage/home.png" alt="이미지" className="img-width"/>
+                </div>
+            </Link>
+            <Link to="/setting">
+                <div className="navbar-icon">
+                    <img src="/images/navbarImage/setting.png" alt="이미지" className="img-width"/>
+                </div>
+            </Link>
         </div>
-        <div className="navbar-icon">
-            <img src="/images/navbarImage/home.png" alt="이미지" className="img-width"/>
-        </div>
-        <div className="navbar-icon">
-            <img src="/images/navbarImage/setting.png" alt="이미지" className="img-width"/>
-        </div>
-    </div>
+    );
 }
