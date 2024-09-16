1422c91c43a0fdaa61c7c13f62b454739a84adb4
jllee000 <jllee000@naver.com>
2024-09-16 21:19:13 +0900

    feat : navbar 아이콘 주소별 분기

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import styles from "./navbar.css";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 export default function Navbar() {
+    const location = useLocation(); // 현재 위치를 얻음
+
     return(
         <div className="navbar-area">
             <Link to= "/dashpage">
-                <div className="navbar-icon">
-                    <img src="/images/navbarImage/chart.png" alt="이미지" className="img-width"/>
+                <div className="navbar-icon dashpage-icon">
+                    <img src={(location.pathname === "/dashpage" || location.pathname.startsWith('/detail/')) 
+                            ? "/images/navbarImage/chart.png" 
+                            : "/images/navbarImage/default-chart.png"}  alt="이미지" className="img-width"/>
                 </div>
             </Link>
             <Link to="/main">
-                <div className="navbar-icon">
-                    <img src="/images/navbarImage/home.png" alt="이미지" className="img-width"/>
+                <div className="navbar-icon main-icon">
+                    <img src={location.pathname === "/main" 
+                            ? "/images/navbarImage/home.png" 
+                            : "/images/navbarImage/default-home.png"} alt="이미지" className="img-width"/>
                 </div>
             </Link>
             <Link to="/setting">
-                <div className="navbar-icon">
-                    <img src="/images/navbarImage/setting.png" alt="이미지" className="img-width"/>
+                <div className="navbar-icon setting-icon">
+                    <img src={location.pathname === "/setting" 
+                            ? "/images/navbarImage/setting.png" 
+                            : "/images/navbarImage/default-setting.png"} alt="이미지" className="img-width"/>
                 </div>
             </Link>
         </div>
