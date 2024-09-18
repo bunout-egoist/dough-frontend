868b3686e164afe5479807791a4bbbe4044cb4c7
jllee000 <jllee000@naver.com>
2024-09-18 13:12:35 +0900

    fix : 배포후 로그인 확인

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.bunout.app',
-  appName: 'bunout',
-  webDir: 'build'
-};
-
-export default config;
