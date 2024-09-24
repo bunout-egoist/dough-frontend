6c9c00a6c177cc9574f1bf45d54db6c42e84271a
jllee000 <jllee000@naver.com>
2024-09-24 16:55:16 +0900

    style : intro tutorial 랜딩 css

--- src/intro/AppleLoginRedirect.jsx
+++ src/intro/AppleLoginRedirect.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect } from "react";
 
 const AppleLoginRedirect = () => {
-    useEffect(() => {
-        // URL에서 form-data 추출
-        const queryParams = new URLSearchParams(window.location.search);
-        const code = queryParams.get('code');
-        const id_token = queryParams.get('id_token');
+    // useEffect(() => {
+    //     // URL에서 form-data 추출
+    //     const queryParams = new URLSearchParams(window.location.search);
+    //     const code = queryParams.get('code');
+    //     const id_token = queryParams.get('id_token');
 
-        // 백엔드 API로 데이터 전송
-        const sendDataToBackend = async () => {
-            const response = await fetch('/api/v1/auth/login/apple', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ code, id_token }), // payload
-            });
+    //     // 백엔드 API로 데이터 전송
+    //     const sendDataToBackend = async () => {
+    //         const response = await fetch('/api/v1/auth/login/apple', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({ code, id_token }), // payload
+    //         });
 
-            if (response.ok) {
-                // 성공적으로 전송된 경우 처리
-                const data = await response.json();
-                console.log('Response from backend:', data);
-            } else {
-                // 오류 처리
-                console.error('Error sending data to backend:', response.statusText);
-            }
-        };
+    //         if (response.ok) {
+    //             // 성공적으로 전송된 경우 처리
+    //             const data = await response.json();
+    //             console.log('Response from backend:', data);
+    //         } else {
+    //             // 오류 처리
+    //             console.error('Error sending data to backend:', response.statusText);
+    //         }
+    //     };
 
-        if (code && id_token) {
-            sendDataToBackend();
-        }
-    }, []);
+    //     if (code && id_token) {
+    //         sendDataToBackend();
+    //     }
+    // }, []);
 
     return (
         <div>
