c6c58009f264192fc646f786e6cc72b920415418
jllee000 <jllee000@naver.com>
2024-09-26 11:49:57 +0900

    feat : 카메라 허용

--- src/main/components/Missionbox.jsx
+++ src/main/components/Missionbox.jsx
@@ -32,7 +32,7 @@ export default function MissionBox({
     try {
       const image = await Camera.getPhoto({
         resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
+        source: CameraSource.Photos,
         quality: 90,
         allowEditing: false,
       });
