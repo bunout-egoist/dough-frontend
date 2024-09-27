f91cbe4f59d4a2892751ab8efe60a08b5bceeeae
jllee000 <jllee000@naver.com>
2024-09-27 13:37:51 +0900

    setting : fcm alarm setting

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging-compat.js')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFDeWiSEbnfiZBStTYuIi2rf0Jc45RSQg",
-    authDomain: "bunout-6fab2.firebaseapp.com",
-    projectId: "bunout-6fab2",
-    storageBucket: "bunout-6fab2.appspot.com",
-    messagingSenderId: "111594277875",
-    appId: "1:111594277875:web:dd8f37f8e9d083f144a086",
-    measurementId: "G-87RHEJHXQ1"
+  apiKey: "AIzaSyAtJWz7IMbcsh65wvTJsr7xzsmpIMDD-Ao",
+  authDomain: "bunout-2ed63.firebaseapp.com",
+  projectId: "bunout-2ed63",
+  storageBucket: "bunout-2ed63.appspot.com",
+  messagingSenderId: "1064933370265",
+  appId: "1:1064933370265:web:3251f0d0af4f04fb170b08",
+  measurementId: "G-ZMM294BFWY"
 };
 
 // Firebase 앱 초기화
@@ -20,10 +20,10 @@ const messaging = firebase.messaging();
 // 백그라운드 메시지 처리
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message ', payload);
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    body: payload.notification.body,
+    icon: '/images/icons/aos.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
--- src/index.js
+++ src/index.js
@@ -24,3 +24,14 @@ if ("serviceWorker" in navigator) {
     });
 }
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((err) => {
+        console.log('Service Worker registration failed:', err);
+      });
+  }
+
