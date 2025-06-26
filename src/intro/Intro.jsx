adfbb157c1cf5aef5ae79298f3e629940e9e0d57
jllee000 <jllee000@naver.com>
2025-06-26 21:52:42 +0900

    fix : fcm 알람 관련 로직 제거

--- src/intro/Intro.jsx
+++ src/intro/Intro.jsx
@@ -3,17 +3,18 @@ import styles from "./intro.css"; // eslint-disable-next-line
 import { isPlatform } from "@ionic/react"; // Import Capacitor's isPlatform utility
 import { Link } from "react-router-dom";
 import FirstPage from "./FirstPage";
-import AppleLogin from "react-apple-login";
+// import AppleLogin from "react-apple-login";
 import IntroPop from "../popup/IntroPop";
 import { useNavigate } from "react-router-dom";
+
 export default function Intro() {
   const [isIos, setIsIos] = useState(false); // State to determine if the device is iOS
   const [showNextPage, setShowNextPage] = useState(false); // State to toggle between FirstPage and NextPage
   const [nonAllow, setNonAllow] = useState(true);
   const navigate = useNavigate();
+
   useEffect(() => {
     requestCameraPermission();
-    requestNotificationPermission();
 
     const checkPlatform = async () => {
       if (isPlatform("ios")) {
@@ -21,12 +22,10 @@ export default function Intro() {
       } else if (isPlatform("android")) {
         setIsIos(false);
       } else {
-        // Fallback: User-Agent 기반으로 macOS 및 iOS 환경을 구분
         const userAgent = window.navigator.userAgent || window.opera;
         if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
           setIsIos(true);
         } else if (/Macintosh/.test(userAgent) && "ontouchend" in document) {
-          // MacBook + 터치 스크린이 있는 경우 (iOS 환경을 모방)
           setIsIos(true);
         } else {
           setIsIos(false);
@@ -35,31 +34,28 @@ export default function Intro() {
     };
 
     checkPlatform();
-    // 페이징 이동
     const timer = setTimeout(() => {
       setShowNextPage(true);
     }, 3000); // 3000ms = 3 seconds
 
-    // Cleanup the timer when the component unmounts
     return () => clearTimeout(timer);
   }, []);
 
   // const REDIRECT_URI = 'http://localhost:3000/oauth2/callback/kakao';
   const REDIRECT_URI = "https://app.bunout.info/oauth2/callback/kakao";
-  const KEY = "8a6e7b4b0b03c895fc6795146375d6ac";
   //process.env.REACT_APP_KEY;
-  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-  console.log("Kakao Client ID:", KEY);
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const loginHandler = () => {
     window.location.href = link;
   };
 
-  const handleAppleResponse = (response) => {
-    console.log("Apple Login Response: ", response);
-    // response.detail.authorization.id_token을 통해 idToken 확인 가능
-    const idToken = response?.authorization?.id_token;
-    console.log("ID Token: ", idToken);
-  };
+  // const handleAppleResponse = (response) => {
+  //   console.log("Apple Login Response: ", response);
+  //   // response.detail.authorization.id_token을 통해 idToken 확인 가능
+  //   const idToken = response?.authorization?.id_token;
+  //   console.log("ID Token: ", idToken);
+  // };
+
   async function requestCameraPermission() {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -72,45 +68,34 @@ export default function Intro() {
       console.log(nonAllow);
     }
   }
-  function requestNotificationPermission() {
-    if ("Notification" in window) {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          console.log("Notifications permission granted");
-          setNonAllow(false);
-        } else {
-          alert("알림을 사용하려면 권한을 허용해주세요.");
-          setNonAllow(true);
-          console.log(nonAllow);
-        }
-      });
-    }
-  }
 
-  const loginWithApple = async (e) => {
-    e.preventDefault();
+  // const loginWithApple = async (e) => {
+  //   e.preventDefault();
 
-    console.log("sign in with apple");
+  //   console.log("sign in with apple");
 
-    window.AppleID.auth.init({
-      clientId: "com.bunout.appServices",
-      redirectURI: "https://app.bunout.info/api/v1/auth/login/apple",
-      state: "bunout",
-      usePopup: true,
-    });
+  //   window.AppleID.auth.init({
+  //     clientId: "com.bunout.appServices",
+  //     redirectURI: "https://app.bunout.info/api/v1/auth/login/apple",
+  //     state: "bunout",
+  //     usePopup: true,
+  //   });
 
-    try {
-      const res = await window.AppleID.auth.signIn();
-      console.log(res);
-      navigate("/redirect/apple", { state: { res } });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //   try {
+  //     const res = await window.AppleID.auth.signIn();
+  //     console.log(res);
+  //     navigate("/redirect/apple", { state: { res } });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
   const handlePopClose = () => {
-    setNonAllow(false); // nonAllow를 false로 설정
+    setNonAllow(false);
   };
+
   const [gifSrc, setGifSrc] = useState("/images/intro/onboard.gif");
+
   return (
     <div className="intropage">
       {nonAllow ? <IntroPop onClose={handlePopClose} /> : <div></div>}
@@ -135,20 +120,11 @@ export default function Intro() {
                   />
                 </div>
               </div>
-              {isIos ? (
+              {/* {isIos ? (
                 <div
                   className="intropage-sns intropage-apple"
                   onClick={loginWithApple}
                 >
-                  {/* <AppleLogin
-                                        clientId="com.bunout.appServices"
-                                        redirectURI="https://app.bunout.info/api/v1/auth/login/apple" 
-                                        responseType="code id_token" // OAuth 2.0에서 사용할 응답 형식
-                                        responseMode="form_post" // 서버로 전달할 방식 (post 요청으로 전달)
-                                        usePopup={false} // 팝업 방식이 아닌 리디렉션 방식 사용
-                                        onSuccess={handleAppleResponse} // 성공 시 실행될 함수
-                                        onError={(error) => console.error('Apple Login Error: ', error)} // 에러 처리
-                                        /> */}
                   <div className="intropage-kakao-img">
                     <img
                       src="/images/intro/apple.png"
@@ -157,7 +133,7 @@ export default function Intro() {
                     />
                   </div>
                 </div>
-              ) : null}
+              ) : null} */}
               <Link to="/tutorial">
                 <div className="landing-tuto">서비스 둘러보기</div>
               </Link>
--- src/intro/Redirection.jsx
+++ src/intro/Redirection.jsx
@@ -1,68 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Capacitor } from "@capacitor/core";
-import { PushNotifications } from "@capacitor/push-notifications";
-import { getMessaging, getToken } from "firebase/messaging";
-import { initializeApp } from "firebase/app";
 
 export default function Redirection() {
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [isNewMember, setIsNewMember] = useState(null);
   const logincode = new URL(window.location.href).searchParams.get("code");
   const navigate = useNavigate();
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAtJWz7IMbcsh65wvTJsr7xzsmpIMDD-Ao",
-    authDomain: "bunout-2ed63.firebaseapp.com",
-    projectId: "bunout-2ed63",
-    storageBucket: "bunout-2ed63.appspot.com",
-    messagingSenderId: "1064933370265",
-    appId: "1:1064933370265:web:3251f0d0af4f04fb170b08",
-    measurementId: "G-ZMM294BFWY",
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const messaging = getMessaging(app);
-
-  const isNativeApp = Capacitor.isNativePlatform();
-
-  const getAndSendToken = async () => {
-    let fcmToken = null;
-    try {
-      if (isNativeApp) {
-        await PushNotifications.requestPermissions();
-        await PushNotifications.register();
-
-        PushNotifications.addListener("registration", (token) => {
-          console.log("푸시 알림 토큰:", token.value);
-          fcmToken = token.value;
-        });
-
-        PushNotifications.addListener("registrationError", (error) => {
-          console.error("푸시 알림 등록 오류:", error);
-        });
-      } else {
-        console.log("웹 환경입니다. FCM을 사용합니다.");
-        const permission = await Notification.requestPermission();
-        if (permission === "granted") {
-          try {
-            fcmToken = await getToken(messaging, {
-              vapidKey:
-                "BKSCCM1MOQZ5KfhXgstx-ZrMJR6P6XRlo3Slb1SI1ct0y4MkeeOZJsTaHkc1o4MUrpk_iHTq1hQtDv2UnUUlibw",
-            });
-            console.log("FCM 토큰:", fcmToken);
-          } catch (error) {
-            console.error("FCM 토큰을 가져오는 중 오류 발생:", error);
-          }
-        } else {
-          console.log("알림 권한이 허용되지 않았습니다.");
-        }
-      }
-    } catch (error) {
-      console.error("Token retrieval error:", error);
-    }
-    return fcmToken;
-  };
 
   const sendLoginRequest = async () => {
     try {
@@ -74,7 +17,6 @@ export default function Redirection() {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            // fcmToken: fcmToken || null,
             fcmToken: null,
           }),
           credentials: "include",
@@ -102,15 +44,9 @@ export default function Redirection() {
 
   useEffect(() => {
     if (logincode) {
-      const handleLogin = async () => {
-        //const fcmToken = await getAndSendToken();
-        await getAndSendToken();
-        await sendLoginRequest();
-      };
-
-      handleLogin();
+      sendLoginRequest();
     }
-  }, [logincode, isNativeApp]);
+  }, [logincode]);
 
   useEffect(() => {
     if (loginSuccess && isNewMember !== null) {
