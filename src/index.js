cfb517c1789ae932992783c2514045802cc6d5cc
jllee000 <jllee000@naver.com>
2025-06-28 16:57:53 +0900

    fix : 해쉬라우터 교체

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import BrowserRouter from "react-router-dom";
 // serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 if ("serviceWorker" in navigator) {
@@ -22,13 +22,13 @@ if ("serviceWorker" in navigator) {
     });
 }
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((err) => {
-      console.log("Service Worker registration failed:", err);
-    });
-}
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/firebase-messaging-sw.js")
+//     .then((registration) => {
+//       console.log("Service Worker registered with scope:", registration.scope);
+//     })
+//     .catch((err) => {
+//       console.log("Service Worker registration failed:", err);
+//     });
+// }
